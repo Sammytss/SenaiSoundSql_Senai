005cb7c16c2f52b37a704b81a34e337f422ad7cf
Sammytss <samuel.tssbr@gmail.com>
2025-04-28 14:53:25 -0300

    Inserindo Interface WEB ao Sistema com o MudBlazor

---
+++ SenaiSoundWEB/Layout/SenaiSoundPalleta.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor.Utilities;
+using MudBlazor;
+
+namespace SenaiSoundWEB.Layout
+{
+    public class SenaiSoundPalleta : PaletteDark
+    { 
+        private SenaiSoundPalleta()
+        {
+            Primary = new MudColor("#9966FF");
+            Secondary = new MudColor("#F6AD31");
+            Tertiary = new MudColor("#8AE491");
+        }
+
+        public static SenaiSoundPalleta CreatePallete => new();
+    }
+}
+
